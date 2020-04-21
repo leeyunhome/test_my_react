7a73be6774c009276e98ab6ebe5cb5cab21f20a0
leeyunhome <leeyunhome@daum.net>
2020-04-22 07:55:57 +0900

    Updates

---
+++ precache-manifest.df17f4d839173a8f00c2f9d7d7804a92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b7de6ab9818cd17939b410ead517e8",
+    "url": "/test_my_react/index.html"
+  },
+  {
+    "revision": "493ab6035dcd194c501f",
+    "url": "/test_my_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b79b72a336010ed2fea7",
+    "url": "/test_my_react/static/js/2.d93eac3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_my_react/static/js/2.d93eac3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493ab6035dcd194c501f",
+    "url": "/test_my_react/static/js/main.8ee5680c.chunk.js"
+  },
+  {
+    "revision": "f6adad74eeb91e6d1d34",
+    "url": "/test_my_react/static/js/runtime-main.02d2fe7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_my_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
